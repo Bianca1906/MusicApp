7255edb8e73bb7d9d7644e4219640b7c2735b917
23Slowpoke <ree.andreeq@gmail.com>
2026-01-15 01:17:44 +0200

    Add files via upload

---
+++ Listener.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace POO_Proiect;
+public class Listener(string nume) : Persoana(nume){ 
+    public List<Playlist> Playlists { get; set; } = new List<Playlist>();
+    public List<Song> MelodiiAp { get; set; } = new List<Song>();
+}
---
+++ Persoana.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace POO_Proiect;
+
+public  abstract class Persoana { 
+    public string Nume { get; set; }
+    public List <Persoana> Followed { get; set; } = new List<Persoana>();
+    public void Follow()
+    {
+        //instructiune follow
+    }
+    public void TopSongs()
+    {
+        //instructiune Top
+    }
+    public void AddPlaylist() { 
+    //instructiune playlist
+    }
+    public void RateAlbum() { 
+    }
+    public Persoana(string nume)
+    {
+        Nume = nume;
+    }
+}
